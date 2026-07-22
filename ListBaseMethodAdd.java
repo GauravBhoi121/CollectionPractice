87e3852de190252cb58573296692a2d5b29a78c3
GauravBhoi <bhoigaurav432@gmail.com>
2026-07-22 11:37:26 +0530

    This Add Method Program Of List Interface

---
+++ ListBaseMethodAdd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class ListBaseMethodAdd
+{
+    public static void main(String args[] )
+    {
+        Scanner sc=new Scanner(System.in);
+        List list=new ArrayList();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println(list);
+    }
+}
