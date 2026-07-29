8d588044090868e4be5933d2bf074f7663a22bfe
GauravBhoi <bhoigaurav432@gmail.com>
2026-07-29 11:27:35 +0530

    StringEquals

--- StringEquals.java
+++ StringEquals.java
@@ -1,41 +1,34 @@
-/*71. Implement your own version of `String.equals()`.
-72. Implement your own version of `String.compareTo()`.
-73. Implement your own version of `String.indexOf()`.
-74. Implement your own version of `String.substring()`.
-75. Implement string compression (Example: aaabbcc → a3b2c2).
-76. Decompress a compressed string.
-77. Implement Run Length Encoding (RLE).
-78. Find all permutations of a string.
-79. Find all combinations of characters in a string.
-80. Find all subsequences of a string.   */
+//71. Implement your own version of `String.equals()`.
 import java.util.*;
 class StringEquals
 {
-    static boolean toEqual(String s,String s2)
-    {       
-          if(s.length()==s2.length())
+     public static boolean equalsOf(String s,String s2)
+     { 
+         if(s.length()!=s2.length())
           {
-             for(int i=0;i<s.length();i++)
-             {
-                 if(s.charAt(i)==s2.charAt(i))
-                  {
-                      continue;
-                  }
-                  else
-                   {
-                          return false;
-                   }
-              }
-              return true;
-             
+             return false;
           }
-          return false;
-      }
-      public static void main(String args[])
-      {
-           String s1="GauravBhoi";
-           String s2="GauravBhoi";
-           Boolean r=toEqual(s1,s2);
-           System.out.println(r);
-      }
-}    
+          for(int i=0;i<s.length();i++)
+          {
+            if(s.charAt(i)!=s2.charAt(i))
+            {
+              return false;
+            }
+          }
+         return true;
+     }
+     public static void main (String arsg[])
+     {
+            Scanner sc=new Scanner(System.in);
+            String s="java";
+            String s2="java";
+            if(equalsOf(s,s2))
+            {
+                 System.out.println("Equals");
+            }
+            else
+            {
+              System.out.println("Not Equals");
+            }
+     }
+}
