a9d8ebb0f01ca476923de797a3f4bdadc2be4dd8
GauravBhoi <bhoigaurav432@gmail.com>
2026-07-31 11:28:10 +0530

    VectoreRemove.java

---
+++ VectoreRemove.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class VectoreRemove
+{
+    public static void main(String args[])
+    {
+         Scanner sc=new Scanner(System.in);
+         Vector v=new Vector();
+         int n=sc.nextInt();
+         System.out.println("Enter the Elemet int the vector");
+         
+         for(int i=0;i<n;)
+         {
+              v.add(sc.nextInt());
+               i++;
+         }
+      
+         System.out.println("Enter the Element to find ");
+         Object o=sc.nextInt();
+         v.remove(o);
+
+            System.out.println(v);
+
+   }
+}
