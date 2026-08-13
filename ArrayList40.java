ab096ba3b01682b79e5e560c16f2c956c48390a9
GauravBhoi <bhoigaurav432@gmail.com>
2026-08-13 16:27:24 +0530

    List program

---
+++ ArrayList40.java
@@ -0,0 +1,16 @@
+/*
+Q40.Problem Statement
+ Write a Java program to store strings in ArrayList and display the string containing maximum vowels.
+Input:
+ ["Apple","Orange","Sky"]
+Output:
+ String with Maximum Vowels: Orange  */
+import java.util.*;
+class ArrayList40
+{
+     public static void main(String args[])
+     {
+          List al=new ArrayList();
+           
+     }
+}
